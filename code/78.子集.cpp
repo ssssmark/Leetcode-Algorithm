91f95bfea5ace421b14ba095fe9ea16e33f6802c
ssssmark <111336914+ssssmark@users.noreply.github.com>
2023-05-31 19:17:23 +0800

    子集II 回溯法去重

--- code/78.子集.cpp
+++ code/78.子集.cpp
@@ -3,7 +3,22 @@
  *
  * [78] 子集
  */
-
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
 // @lc code=start
 class Solution {
 public:
---
+++ code/90.子集-ii.cpp
@@ -0,0 +1,65 @@
+/*
+ * @lc app=leetcode.cn id=90 lang=cpp
+ * @lcpr version=21908
+ *
+ * [90] 子集 II
+ */
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lc code=start
+class Solution {
+private:
+    vector<vector<int>> result;
+    vector<int> path;
+    void backtracking(vector<int>& nums, int startIndex) {
+        result.push_back(path);
+        unordered_set<int> uset;
+        for (int i = startIndex; i < nums.size(); i++) {
+            if (uset.find(nums[i]) != uset.end()) {
+                continue;
+            }
+            uset.insert(nums[i]);
+            path.push_back(nums[i]);
+            backtracking(nums, i + 1);
+            path.pop_back();
+        }
+    }
+
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        result.clear();
+        path.clear();
+        sort(nums.begin(), nums.end()); // 去重需要排序
+        backtracking(nums, 0);
+        return result;
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,2,2]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [0]\n
+// @lcpr case=end
+
+ */
+
